4279a9e8671ac0e9f6013b4a13eeefab5e41940a
Ben Livermore <blivermore@wespire.com>
2020-05-17 12:17:10 -0400

    Add files via upload

---
+++ jasmine-test-structure.js
@@ -0,0 +1,13 @@
+describe('function to be tested name', () => {
+  it('should explain first test case', () => {
+    // run your function
+    const result = myFunction();
+    
+    //test the result
+    expect(result).toBe(expectedValue)
+  });
+
+  it('should explain second test case', () => {
+    // ...run and test second case here
+  });
+});
